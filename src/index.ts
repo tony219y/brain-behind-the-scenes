9fc2ad82d0daee11d886269cd0e8d4fd98508f87
tony007x <aekanan2004@gmail.com>
2024-12-06 22:25:44 +0700

    userfolder

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import register from './api/v1/auth/authorization.js'
+import user from './api/v1/users/user.js'
 
 const app = new Hono()
 
 app.route('/api/v1/auth',register)
-// app.route('/api/v1/users',users)
+app.route('/api/v1/users',user)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
