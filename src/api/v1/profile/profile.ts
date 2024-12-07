f00dd1b4b1dd675f448a7a622d715ca10f0df3f0
peakp <peakpeakpeakhaha@gmail.com>
2024-12-07 16:52:35 +0700

    try profile

--- src/api/v1/profile/profile.ts
+++ src/api/v1/profile/profile.ts
@@ -1,3 +1,9 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
+
+app.post('/profile', async (c) => {
+
+})
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import register from './api/v1/auth/authorization.js'
 import user from './api/v1/users/user.js'
+import profile from './api/v1/profile/profile.js'
 
 const app = new Hono()
 
 app.route('/api/v1/auth',register)
 app.route('/api/v1/users',user)
+app.route('/api/v1/profile',profile)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
