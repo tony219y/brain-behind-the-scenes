dc89a495aa679c118329d7a6d4938db63dd91984
peakp <peakpeakpeakhaha@gmail.com>
2024-12-07 17:58:01 +0700

    try input name

--- src/api/v1/profile/profile.ts
+++ src/api/v1/profile/profile.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono();
 
 app.post('/profile', async (c) => {
+    const {name} = await c.req.json();
+    console.log(name + " from back");
 
+    return c.json({message : name}, 200)
 })
 
 export default app;
