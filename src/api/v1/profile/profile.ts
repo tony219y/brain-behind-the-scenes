7a44b97191168bf407bc6030a84bcfba63cf34ed
peakp <peakpeakpeakhaha@gmail.com>
2024-12-08 12:31:59 +0700

    profile put

--- src/api/v1/profile/profile.ts
+++ src/api/v1/profile/profile.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 
 const app = new Hono();
 
-app.post('/profile', async (c) => {
+app.put('/profile', async (c) => {
     const {name} = await c.req.json();
     console.log(name + " from back");
 
