73d1c936be62ee118ffe5373ee160e8e90f77f21
tony007x <aekanan2004@gmail.com>
2024-12-10 16:16:13 +0700

    send username to post

---
+++ drizzle/0005_dear_steve_rogers.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "Users" ADD COLUMN "skill" varchar(255);--> statement-breakpoint
+ALTER TABLE "Users" ADD COLUMN "education" varchar(255);--> statement-breakpoint
+ALTER TABLE "Users" ADD COLUMN "facebook" varchar(255);--> statement-breakpoint
+ALTER TABLE "Users" ADD COLUMN "github" varchar(255);--> statement-breakpoint
+ALTER TABLE "Users" ADD COLUMN "x" varchar(255);
